038b11b587369a3bc8d215d8e4a4f36446ee2c65
Erich Grunewald <erichgrunewald@gmail.com>
2021-02-27 22:38:25 +0100

    Implement email sending

--- newsletter/newsletter.py
+++ newsletter/newsletter.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from typing import Optional
 import email
 import imaplib
 import json
@@ -82,6 +85,7 @@ def sync_subscribers(config: Config) -> list[str]:
             click.echo(f"subscribe {sender}")
             subscribers.append(sender_email)
 
+    click.echo(f"Got {len(subscribers)} active subscriber(s)")
     return subscribers
 
 @cli.command()
@@ -128,36 +132,92 @@ def ensure_config(config):
             "Config file contains empty value(s) (use \"nwsl configure\" to change it)",
         )
 
+def send_email_func(config: Config,
+                    html_text: Optional[str],
+                    plain_text: Optional[str],
+                    subject: str,
+                    subscribers: list[str],
+                    dry_run: bool):
+    def find_title_in_html(html: str) -> Optional[str]:
+        result = re.search(r"<h1>(.+)<\/h1>", html)
+        return result.group(1) if result else None
+
+    def find_title_in_markdown(md: str) -> Optional[str]:
+        result = re.search(r"# (.+)", md)
+        return result.group(1) if result else None
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(config.host, 465, context=context) as server:
+        # TODO: Get password from user input, not config file.
+        server.login(config.user, config.password)
+
+        title = find_title_in_html(html_text) if html_text else find_title_in_markdown(plain_text) or 'Untitled'
+
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject or title
+        # TODO: Add newsletter name to config.
+        msg['From'] = f"Newsletter name <{config.user}>"
+        # TODO: Allow different sender email from IMAP user.
+        msg['To'] = config.user
+
+        if plain_text:
+            msg.attach(MIMEText(plain_text, 'plain'))
+        if html_text:
+            msg.attach(MIMEText(html_text, 'html'))
+
+
+        click.echo(f"\nWant to send out newsletter to {len(subscribers)} subscriber(s):\n\n" +
+                   f"{(plain_text or html_text)[:300]} ...\n")
+        if plain_text and html_text:
+            click.echo(f"HTML body:\n\n{html_text[:300]} ...\n");
+
+        if not dry_run and click.confirm('Do you want to proceed?'):
+            server.sendmail(config.user, subscribers, msg.as_string())
+            click.echo(f"Sent \"{title}\" to {len(subscribers)} subscriber(s)")
+        elif dry_run:
+            click.echo(f"Would have sent \"{title}\" to {len(subscribers)} subscriber(s)")
+        else:
+            click.echo(f"Did not send \"{title}\"")
+
+        server.quit()
+
+
 @cli.command()
-@click.argument('filepath')
+@click.argument('filepath', type=click.File('r'))
+@click.argument('alternative', type=click.File('r'), required=False)
+@click.option('--subject', type=str)
+@click.option('--dry-run', is_flag=True)
 @click.pass_obj
-def send_email(config, filepath):
-    """Sends email at given path to all subscribers."""
+def send_email(config, filepath, alternative, subject, dry_run):
+    """Send email with content at given path(s) to all subscribers."""
     ensure_config(config)
 
     subscribers = sync_subscribers(config)
     click.echo(subscribers)
+    
+    def is_html(filepath: str):
+        return filepath.endswith('.html')
 
-    # Create a secure SSL context.
-    context = ssl.create_default_context()
 
-    # TODO: Split email reading & sending out into a separate service.
-    with smtplib.SMTP_SSL(config.host, 465, context=context) as server:
-        server.login(config.user, config.password)
+    is_filepath_html = is_html(filepath.name)
+    is_alternative_html = alternative and is_html(alternative.name)
+    if alternative:
+        if is_filepath_html and is_alternative_html:
+            raise click.UsageError(
+                "Both files are HTML; you should provide 1 HTML file and 1 plain text file"
+            )
+        if not is_filepath_html and not is_alternative_html:
+            raise click.UsageError(
+                "Neither file is HTML; you should provide 1 HTML file and 1 plain text file"
+            )
 
-        msg = email.message.EmailMessage()
-        msg['Subject'] = 'Hello again!'
-        msg['From'] = f"Newsletter name <{config.user}>"
-        msg['To'] = config.user
-        msg['BCC'] = subscribers
+    body = filepath.read()
+    if not alternative:
+        plain_text = None if is_filepath_html else body
+        html_text = body if is_filepath_html else None
+    else:
+        alt_body = alternative.read()
+        plain_text = alt_body if is_filepath_html else body
+        html_text = body if is_filepath_html else alt_body
 
-        # TODO: Use input file, which can be either .txt or .html.
-        msg.set_content("This is my newsletter, hi!")
-
-        # TODO: Get password from user input, not config file.
-
-        if click.confirm(f"Do you want to send this mail to {len(subscribers)} subscriber(s)?"):
-            server.send_message(msg)
-            click.echo(f"Sent {filepath} to {len(subscribers)} subscriber(s)")
-
-        server.close()
+    send_email_func(config, html_text, plain_text, subject, subscribers, dry_run)
